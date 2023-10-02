68fef63f8dfa2b87e0cc48c7b70aa1a54c59b26f
Chayki Kanevski <126410874+Chaya185@users.noreply.github.com>
2023-10-02 12:26:14 -0400

    Modified to use iterator

--- number4.java
+++ number4.java
@@ -1,12 +1,41 @@
-package HW_3;
+package HW_2;
 
 import java.util.*;
+
+class CustomCollection {
+    private Integer[] elements;
+
+    public CustomCollection(Integer[] elements) {
+        this.elements = elements;
+    }
+
+    public Iterator<Integer> iterator(){
+        return new CustomIterator();
+    }
+
+    private class CustomIterator implements Iterator<Integer>{
+        private int currentIndex = 0;
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex < elements.length;
+        }
+
+        @Override
+        public Integer next() {
+            if (!hasNext()) {
+                throw new java.util.NoSuchElementException();
+            }
+            return elements[currentIndex++];
+        }
+    }
+}
 
 public class number4 {
     public static void main(String[] args){
         Integer[] numbers = {1,2,3,4,5,6,7,8,9,10};
-        List<Integer> numberList = Arrays.asList(numbers);
-        Iterator<Integer> iterator = numberList.iterator();
+        CustomCollection c = new CustomCollection(numbers);
+        Iterator<Integer> iterator = c.iterator();
         while(iterator.hasNext()){
             Integer number = iterator.next();
             System.out.println(number);
